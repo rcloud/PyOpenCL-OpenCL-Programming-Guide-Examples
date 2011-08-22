3b19ec36982732b2f75c6bf95eeb6384c285169a
Robert Cloud <rcloud@gmail.com>
2011-08-22 10:54:53 -0500

    working

--- laplace/jacobian-2d.py
+++ laplace/jacobian-2d.py
@@ -12,33 +12,31 @@ We use the CPU as the primary target device in this code
 
 import pyopencl as cl
 import numpy as np
+import time
+size = 1000
 
-size = 5
-"""
-OpenCL only offers single dimensional arrays as buffers but
-our solver operates in two dimensional space.  We create 
-a macro to get the value u[i][j] i being the row and j 
-being the column in the matrix
-"""
+width_macro = "#define WIDTH %s" %str(size)
 
-macro = "#define val(u,y,x) (u[y * %s + x])" %str(size)
-kernel_source = macro + """
+kernel_source = width_macro + """
 __kernel void solve(__global float *u, 
                     __global float *u_new)
                     {
-                    int y = get_global_id(0);
-                    int x = get_global_id(1);
 
-                    int width = get_global_size(0);
-                    if (y != 0 && y != width - 1 && x != 0 && x != width - 1)
+                    int id = get_global_id(0);
+
+                    int y = id / WIDTH;
+                    int x = id % WIDTH;
+
+                    if (y != 0 && y != WIDTH - 1 && x != 0 && x != WIDTH - 1)
                     {
-                    val(u_new, y, x) = (val(u, y + 1, x) + 
-                                        val(u, y - 1, x) + 
-                                        val(u, y, x + 1) +
-                                        val(u, y, x - 1)) / 4;
+                    u_new[y * WIDTH + x] = (u[(y + 1) * WIDTH + x] +
+                                           u[(y - 1) * WIDTH + x] + 
+                                           u[y * WIDTH + (x + 1)] + 
+                                           u[y * WIDTH + (x - 1)]) / 4;
                     }
                     }
 """
+
 def initialize(a):
     #initialized using Chapra's values, see README
     a[0,:] = 100 #top row
@@ -59,19 +57,19 @@ u_new=np.copy(u)
 program = cl.Program(ctx, kernel_source).build()
 queue = cl.CommandQueue(ctx)
 
-u_dev = cl.array.to_device(queue, u)
-u_dev_new = cl.array.to_device(queue, u_new)
 
-"""
 mf = cl.mem_flags
 #create the memory objects on the device
 u_dev = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=u)
 u_new_dev = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=u_new)
-"""
 
-#program.solve(queue, u.shape, None, u_dev, u_new_dev)
-
-#cl.enqueue_copy(queue, u_new, u_new_dev)
-#print u_new
+iteration = 0
 
 
+for iteration in range(1000):
+    if iteration % 2 == 0:
+        program.solve(queue, (size * size,), None, u_dev, u_new_dev)
+    else:
+        program.solve(queue, (size * size,), None, u_new_dev, u_dev)
+
+cl.enqueue_copy(queue, u_new, u_new_dev)
